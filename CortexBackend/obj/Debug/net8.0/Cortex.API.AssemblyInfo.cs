b98babb470333f86704e56e2c303ad0e31c4fc16
airsicktitan <adamcwhooper@yahoo.com>
2026-04-13 01:51:29 -0400

    Update workflow

--- CortexBackend/obj/Debug/net8.0/Cortex.API.AssemblyInfo.cs
+++ CortexBackend/obj/Debug/net8.0/Cortex.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cortex.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe25f242622f272f0d7f56bc733696aed8ccb21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b457335f8ee292139834984ebe0cb54c9a28dc80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cortex.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cortex.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
